88260a3174e4239fb488a560824f4f17638a9d4f
Marcus <56905193+marcustkl@users.noreply.github.com>
2022-03-01 23:18:38 +0800

    added stream to chip script (not working yet)

--- stream_acc_gyro_to_chip.py
+++ stream_acc_gyro_to_chip.py
@@ -8,18 +8,39 @@ from threading import Event
 from queue import Queue
 from copy import deepcopy
 
+import os
+import argparse
+import serial
+import struct
 import platform
 import sys
+
+# Start - Match these with MAX78000 application
+SERIAL_BAUD = 115200
+DATA_LEN = 4
+# End
+
+SERIAL_TOUT = 5 # Seconds
+
+# Host Functions
+def print_result(filename, result):
+    """ Print formatted result """
+    print("{0}\t{1}".format(filename, result), end = '')
+
+def send_readings(sport, readings: list[float]):
+        sport.write(struct.pack('ffffff', readings))
 
 # Sensor Classes and Functions
 class State:
     # init state
-    def __init__(self, device):
+    def __init__(self, device, sport):
         self.device = device
         self.samples = 0
         self.accCallback = FnVoid_VoidP_DataP(self.acc_data_handler)
         self.gyroCallback = FnVoid_VoidP_DataP(self.gyro_data_handler)
         self.readings = Queue()
+        self.sport = sport
+        self.results = []
 
     # acc callback function
     def acc_data_handler(self, ctx, data):
@@ -37,8 +58,27 @@ class State:
         # round to 6 dp
         value_acc_gyro = [round(num, 6) for num in value_acc_gyro]
         print("ACC: %s GYRO: %s" % (value_acc_gyro[0:3], value_acc_gyro[3:]))
+
+        # send readings to MAX78000
+        send_readings(self.sport, value_acc_gyro)
+
         print("--------------------")
         self.samples+= 1
+
+# init uart serial interface
+
+#  parse arguments
+parser = argparse.ArgumentParser(description = "Test sending and receiving data from MAX78000 UART",
+                                     formatter_class = argparse.RawTextHelpFormatter)
+parser.add_argument("-d", "--device",
+                    required = True,
+                    help = "Serial device connected to MAX78000.\n"
+                            "Linux: /dev/ttyUSB0\n"
+                            "Windows: COM3")
+args = parser.parse_args()
+
+# Open device with default description
+sport = serial.Serial(args.device, SERIAL_BAUD, timeout = SERIAL_TOUT)
 
 # init sensor
 
